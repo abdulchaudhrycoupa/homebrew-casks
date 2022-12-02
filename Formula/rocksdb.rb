3707b5544c06a483f41e2643e01937a60ef85ab3
Abdul Chaudhry <abdul.chaudhry@coupa.com>
2022-12-02 11:57:33 -0800

    added rocksdb 6.29.3

---
+++ Formula/rocksdb.rb
@@ -0,0 +1,133 @@
+class Rocksdb < Formula
+  desc "Embeddable, persistent key-value store for fast storage"
+  homepage "https://rocksdb.org/"
+  url "https://github.com/facebook/rocksdb/archive/v6.29.3.tar.gz"
+  sha256 "724e4cba2db6668ff6a21ecabcce0782cd0c8e386796e7e9a14a8260e0600abd"
+  license any_of: ["GPL-2.0-only", "Apache-2.0"]
+  head "https://github.com/facebook/rocksdb.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "8c0d4c202bd8ee00b7967c9638b3840e6f0f8696136bec1dff36d1754911955f"
+    sha256 cellar: :any,                 arm64_big_sur:  "42593ec6b59bb39c1a105b8e2e03007b02863b712d05679a30a2238e07700fd4"
+    sha256 cellar: :any,                 monterey:       "a4d7816c6785841657b150a690c39ca02f48c626b5b3488ad52b3b14133e4088"
+    sha256 cellar: :any,                 big_sur:        "a546cbbf5a924512df6f9a9745c1ee0a01b72fbe17f6795b8180d09104bd272b"
+    sha256 cellar: :any,                 catalina:       "a561cfe15fd4b83ac0ccaed44135fba3d4086937bc39150a819a801ede42d9f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5e506b56e50c7f8a42f06bfe2d3762e9d3d4c574a7823a18b5caf80f967fb126"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "gflags"
+  depends_on "lz4"
+  depends_on "snappy"
+  depends_on "zstd"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  def install
+    ENV.cxx11
+    base_args = std_cmake_args + %W[
+      -DPORTABLE=ON
+      -DUSE_RTTI=ON
+      -DWITH_BENCHMARK_TOOLS=OFF
+      -DWITH_BZ2=ON
+      -DWITH_LZ4=ON
+      -DWITH_SNAPPY=ON
+      -DWITH_ZLIB=ON
+      -DWITH_ZSTD=ON
+      -DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,#{rpath}
+    ]
+
+    # build rocksdb_lite
+    lite_args = base_args + %w[
+      -DROCKSDB_LITE=ON
+      -DARTIFACT_SUFFIX=_lite
+      -DWITH_CORE_TOOLS=OFF
+      -DWITH_TOOLS=OFF
+    ]
+    mkdir "build_lite" do
+      system "cmake", "..", *lite_args
+      system "make", "install"
+    end
+    p = lib/"cmake/rocksdb/RocksDB"
+    ["Targets.cmake", "Targets-release.cmake"].each do |s|
+      File.rename "#{p}#{s}", "#{p}_Lite#{s}"
+    end
+
+    # build regular rocksdb
+    mkdir "build" do
+      system "cmake", "..", *base_args
+      system "make", "install"
+
+      cd "tools" do
+        bin.install "sst_dump" => "rocksdb_sst_dump"
+        bin.install "db_sanity_test" => "rocksdb_sanity_test"
+        bin.install "write_stress" => "rocksdb_write_stress"
+        bin.install "ldb" => "rocksdb_ldb"
+        bin.install "db_repl_stress" => "rocksdb_repl_stress"
+        bin.install "rocksdb_dump"
+        bin.install "rocksdb_undump"
+      end
+      bin.install "db_stress_tool/db_stress" => "rocksdb_stress"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <assert.h>
+      #include <rocksdb/options.h>
+      #include <rocksdb/memtablerep.h>
+      using namespace rocksdb;
+      int main() {
+        Options options;
+        return 0;
+      }
+    EOS
+
+    extra_args = []
+    on_macos do
+      extra_args << "-stdlib=libc++"
+      extra_args << "-lstdc++"
+    end
+    system ENV.cxx, "test.cpp", "-o", "db_test", "-v",
+                                "-std=c++11",
+                                *extra_args,
+                                "-lz", "-lbz2",
+                                "-L#{lib}", "-lrocksdb",
+                                "-L#{Formula["snappy"].opt_lib}", "-lsnappy",
+                                "-L#{Formula["lz4"].opt_lib}", "-llz4",
+                                "-L#{Formula["zstd"].opt_lib}", "-lzstd"
+    system "./db_test"
+    system ENV.cxx, "test.cpp", "-o", "db_test_lite", "-v",
+                                "-std=c++11",
+                                *extra_args,
+                                "-lz", "-lbz2",
+                                "-L#{lib}", "-lrocksdb_lite",
+                                "-DROCKSDB_LITE=1",
+                                "-L#{Formula["snappy"].opt_lib}", "-lsnappy",
+                                "-L#{Formula["lz4"].opt_lib}", "-llz4",
+                                "-L#{Formula["zstd"].opt_lib}", "-lzstd"
+    system "./db_test_lite"
+
+    assert_match "sst_dump --file=", shell_output("#{bin}/rocksdb_sst_dump --help 2>&1")
+    assert_match "rocksdb_sanity_test <path>", shell_output("#{bin}/rocksdb_sanity_test --help 2>&1", 1)
+    assert_match "rocksdb_stress [OPTIONS]...", shell_output("#{bin}/rocksdb_stress --help 2>&1", 1)
+    assert_match "rocksdb_write_stress [OPTIONS]...", shell_output("#{bin}/rocksdb_write_stress --help 2>&1", 1)
+    assert_match "ldb - RocksDB Tool", shell_output("#{bin}/rocksdb_ldb --help 2>&1")
+    assert_match "rocksdb_repl_stress:", shell_output("#{bin}/rocksdb_repl_stress --help 2>&1", 1)
+    assert_match "rocksdb_dump:", shell_output("#{bin}/rocksdb_dump --help 2>&1", 1)
+    assert_match "rocksdb_undump:", shell_output("#{bin}/rocksdb_undump --help 2>&1", 1)
+
+    db = testpath / "db"
+    %w[no snappy zlib bzip2 lz4 zstd].each_with_index do |comp, idx|
+      key = "key-#{idx}"
+      value = "value-#{idx}"
+
+      put_cmd = "#{bin}/rocksdb_ldb put --db=#{db} --create_if_missing --compression_type=#{comp} #{key} #{value}"
+      assert_equal "OK", shell_output(put_cmd).chomp
+
+      get_cmd = "#{bin}/rocksdb_ldb get --db=#{db} #{key}"
+      assert_equal value, shell_output(get_cmd).chomp
+    end
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Abdulchaudhrycoupa Casks
+
+## How do I install these formulae?
+
+`brew install abdulchaudhrycoupa/casks/<formula>`
+
+Or `brew tap abdulchaudhrycoupa/casks` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
